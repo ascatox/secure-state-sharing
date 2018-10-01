d15ccf407abaafce91d1146fa6bf68096c8f2a20
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-10-01 14:52:38 +0200

    Improvements...

---
+++ secureStateController.js
@@ -0,0 +1,84 @@
+const BlockchainHandler = require('./lib/blockchainHandler');
+const blockchainHandler = new BlockchainHandler();
+const RequestHandler = require('./lib/requestHandler');
+const requestHandler = new RequestHandler();
+const OrionHandler = require('./lib/orionHandler');
+const orionHandler = new OrionHandler();
+
+class SecureStateController {
+
+    constructor() {}
+
+
+    async executeRequest(id, type, requestType) {
+        let entity, entityOld = null;
+        try {
+            entity = await orionHandler.getEntity(id, type);
+            if (entity && entity.hasOwnProperty('entity'))
+                entity = entity.entity;
+            else {
+                if (this.isMigration(id, type)) {
+                    entity = await blockchainHandler.getEntity(id, type);
+                    orionHandler.createContext(entity);
+                } else
+                    console.error('Context not present in Orion');
+            }
+            entityOld = JSON.parse(JSON.stringify(entity));
+            //Orion has already build the context local, build the Master
+            await orionHandler.createMasterContext(id, type);
+            const isUpdate = await this.isUpdate(requestType, entity);
+            const result = await blockchainHandler.editEntity(entity, isUpdate);
+            if (result) {
+                const txId = rez.tx_id.getTransactionID();
+                blockchainHandler.registerTxEvent(txId, this.onEvent, this.onError);
+            }
+        } catch (error) {
+            console.error(error);
+            if (entityOld)
+                orionHandler.updateEntity(entityOld);
+            throw new Error(error);
+        }
+    }
+
+
+    async onEvent(transactionId) {
+        if (transactionId) {
+            console.log('Transaction ' + transactionId + ' correctly committed to the chain.');
+            let entityUpd = await blockchainHandler.getEntity(entity.id, entity.type);
+            const result = await orionHandler.updateEntityMasterFromChain(entityUpd);
+            console.log("Update executed by Blockchain with OCB updated!!!\nFinal entity ->\n" +
+                JSON.stringify(result.entity));
+        }
+
+    }
+
+
+    async onError(error) {
+        console.error(('Error received in transaction:  with error: ' + error));
+        throw new Error(err);
+    }
+
+    async isMigration(id, type) {
+        const result = await blockchainHandler.getEntity(id, type);
+        if (result) return true;
+        return false;
+    }
+
+    async isUpdate(requestType, entity) {
+        const isUpdate = requestType === 'PUT';
+        const result = await blockchainHandler.getEntity(entity.id, entity.type);
+        if (!isUpdate) { //create
+            if (result)
+                throw new Error('Could not create entity already present in Blockchain');
+            return false;
+        } else { //Update
+            if (!result) {
+                throw new Error('Could not update entity not present in Blockchain');
+                return true;
+            }
+        }
+    }
+
+}
+
+module.exports = SecureStateController;
--- server.js
+++ server.js
@@ -7,31 +7,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const BlockchainHandler = require('./lib/blockchainHandler');
-const blockchainHandler = new BlockchainHandler();
+const SecureStateController = require('./secureStateController');
+const secureStateController = new SecureStateController();
+
 const RequestHandler = require('./lib/requestHandler');
 const requestHandler = new RequestHandler();
-const OrionHandler = require('./lib/orionHandler');
-const orionHandler = new OrionHandler();
 
 let timeoutId;
 app.use(errorHandler);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
     extended: true
-})); // for parsing application/x-www-form-urlencoded
-//app.use('/', proxy({target: OCB_URL}));
-
-
+}));
 
 
 app.all("/*", function (req, res) {
     console.log('redirecting to Orion Context Broker');
-
-    // apiProxy.on('error', function (e) {
-    //     clearTimeout(timeoutId);
-    //     next(e);
-    // });
     try {
         if (req.path.indexOf('v1') >= 0) {
             res.status(400).send("Warning!!! v1 APIs not supported use v2 -> http://telefonicaid.github.io/fiware-orion/api/v2/stable/");
@@ -46,38 +37,13 @@ app.all("/*", function (req, res) {
             return;
             //Editing mode of OCB    
         } else if (requestHandler.isOnBehalfOfChain(req)) { //I'm updating attributes
+            const id = requestHandler.getId(req);
+            const type = requestHandler.getType(req);
             const run = async () => {
-                try {
-                    let entity = await orionHandler.getEntity(requestHandler.getId(req), requestHandler.getType(req));
-                    if (entity && entity.hasOwnProperty('entity'))
-                        entity = entity.entity;
-                    //requestHandler.validateEntity(entity);
-                    await orionHandler.createMasterContext(entity.id, entity.type);
-                    blockchainHandler.updateByBlockchain(entity, requestHandler.isUpdate(req)).then((result) => {
-                        if (result)
-                            console.log("Update correclty executed with result\n" + JSON.stringify(result));
-                    }).catch(error => {
-                        console.error('Error encountered : ' + JSON.stringify(error));
-                    });
-                } catch (error) {
-                    console.error(error);
-                }
+                secureStateController.executeRequest(id, type, req.method);
             };
             timeoutId = setTimeout(run, CONFIG.timeout);
-            /*res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.write('request successfully proxied to Orion Context Broker -> ' + OCB_URL + '\n' + req.method + ": " + req.path + "\n" +
-                JSON.stringify(req.headers, true, 2));
-            res.end();
-            */
         }
-        /*return apiProxy.web(req, res, {
-            target: OCB_URL,
-            changeOrigin: true,
-            toProxy: false,
-            prependPath: false
-        });*/
         return res.redirect(307, OCB_URL + req.path);
     } catch (error) {
         console.error(error);
