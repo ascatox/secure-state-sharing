d8fd080177e4c281e621ddbfc09f3cfce2d54a81
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-11-23 09:31:36 +0100

    Fix

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -4,5 +4,7 @@ if [ "$1" == "" ]; then
 	echo "Insert the version of your package: '1.0.0'"
 	exit 0
 fi
+rm -rf ../resources
+mv ../resources-docker ../resources 
 docker build -t faredge/secure-state-sharing:$1 .
 
